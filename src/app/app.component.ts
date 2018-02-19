a5ea7e4b11f316adcd827a95e595f0f18a451de0
Olli Saarinen <osaarine93@gmail.com>
2018-02-19 18:50:47 +0200

    Small add with comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { NotificationService } from '@app/core/services/notification.service';
 import { AuthService, UserService } from '@app/shared/services';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,8 @@ import { AuthService, UserService } from '@app/shared/services';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  data$: Observable<any>;
 
   constructor(private userService: UserService,
     private auth: AuthService,
@@ -28,6 +31,7 @@ export class AppComponent {
   }
 
   moi() {
-    this.userService.test(12).subscribe((x) => console.log(x));
+    this.data$ = this.userService.test(101010);
+    //this.userService.test(12).subscribe((x) => console.log(x));
   }
 }
--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,15 +1,21 @@
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
 
 import { Injectable } from '@angular/core';
 import { AppUser } from '@app/shared/models';
 import { AngularFireDatabase } from 'angularfire2/database';
 import * as firebase from 'firebase';
 import { Observable } from 'rxjs/Observable';
+import { _throw } from 'rxjs/observable/throw';
 
 export interface Data {
   id: string;
   a: string;
   b: string;
+  c?: number;
+  d?: number;
 }
 
 @Injectable()
@@ -32,38 +38,65 @@ export class UserService {
   }
 
   test(id: number): Observable<Data> {
-    const d = {} as Data;
-    return this.obs('id')
+    return this.obs(id.toString())
+      // Eka request
       .map(eka => ({ id: eka } as Data))
-      .catch(() => {
-        console.log('error');
+      // Eka error
+      .catch((e) => {
+        console.log(e);
         return Observable.of({});
       })
+      // Toka request
       .switchMap((data: Data) => {
-        return this.obs('toka')
+        console.log(data);
+        return this.obs('12')
           .map(toka => {
-            console.log(data);
             data.a = toka;
             return data;
           });
       })
-      .catch(() => {
-        console.log('error');
+      // ...
+      .catch((e) => {
+        console.log(e);
+        // return _throw('toka');
         return Observable.of({});
       })
+      // ...
       .switchMap((data: Data) => {
-        return this.obs('X')
+        return this.obs('32')
           .map(kolmas => ({ b: kolmas, ...data }));
       })
-      .catch(() => {
-        console.log('error 2');
+      // ...
+      .catch((e) => {
+        console.log(e);
+        // return _throw('kolmas');
+        return Observable.of({});
+      })
+      // ...
+      .switchMap((data: Data) => {
+        console.log(data);
+        return this.obs('2X')
+          .map(neljas => ({
+            c: neljas.c,
+            d: neljas.d,
+            ...data
+          }));
+      })
+      .do(data => console.log(data))
+      .catch((e) => {
+        console.log(e);
         return Observable.of({} as Data);
       });
   }
 
-  private obs(a: string): Observable<string> {
-    if (a === 'X') {
-      Observable.throw('virhe');
+  // Observable generator
+  private obs(a: string): Observable<any> {
+    // Simulate error
+    if (a === 'e') {
+      return _throw('virhe');
+    }
+    if (a === '2X') {
+      return Observable.of({ c: 1, d: 2, e: 5 });
     }
     return Observable.of(a);
   }
